2d7f9e659041243d68827afcdfdbc9b385339edd
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2019-06-27 13:30:05 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     //{ path: '', redirectTo: 'menu', pathMatch: 'full' },
  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  //{ path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   //{ path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
   //{ path: 'entrega', loadChildren: './pages/entrega/entrega.module#EntregaPageModule' },
   //{ path: 'inventario', loadChildren: './pages/inventario/inventario.module#InventarioPageModule' },
