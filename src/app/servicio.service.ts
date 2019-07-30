f776ccf33e73672e4e9453d0640b8575f9990a31
majimenezso <alejo-taizon@hotmail.com>
2019-07-30 11:25:41 -0500

    Revert "n"
    This reverts commit 244d5490d61326eb2882f2b1c21ce31cb4c1b9f0.

---
+++ src/app/servicio.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioService {
+
+
+  constructor(public http: HttpClient) {
+    console.log("hello service");
+   }
+   Obtenerdatos(){
+    return this.http.get('http://localhost/rol:3306');
+    console.log('Datos');
+   }
+}
