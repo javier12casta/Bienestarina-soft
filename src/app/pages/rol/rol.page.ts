a34f072b6651c51c5fc2b717f8f922858087d54f
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2019-07-31 04:57:45 -0500

    loading-toast

--- src/app/pages/rol/rol.page.ts
+++ src/app/pages/rol/rol.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicioService } from '../../servicio.service';
-import {AlertController, ToastController, LoadingController} from '@ionic/angular';
+import { AlertController, ToastController, LoadingController } from '@ionic/angular';
 import { Rol } from 'src/app/interfaces/rol';
+import { async } from 'q';
 
 @Component({
   selector: 'app-rol',
@@ -9,40 +10,40 @@ import { Rol } from 'src/app/interfaces/rol';
   styleUrls: ['./rol.page.scss'],
 })
 export class RolPage implements OnInit {
-  usuarios: Rol [] = [];
-  constructor( 
-    private Service: ServicioService, 
-    private alertCtrl: AlertController, 
+  usuarios: Rol[] = [];
+  constructor(
+    private Service: ServicioService,
+    private alertCtrl: AlertController,
     private toastCtrl: ToastController,
     private loadingCtrl: LoadingController
-    ) {}
+  ) { }
 
-  async openAlert () {
+  async openAlert() {
     const alert = await this.alertCtrl.create({
       header: 'Crear un nuevo rol',
-      inputs:[
+      inputs: [
         {
           name: 'RolPersona',
           type: 'text',
-          placeholder:'Tipo de Rol',
+          placeholder: 'Tipo de Rol',
         },
         {
           name: 'Estado',
           type: 'number',
-          placeholder:`Estado 1 Activo, 0 Inactivo`,
+          placeholder: `Estado 1 Activo, 0 Inactivo`,
         },
       ],
       buttons: [
         {
           text: 'Cancelar',
-          role:'cancel',
+          role: 'cancel',
           cssClass: 'secondary',
         },
         {
           text: 'Crear',
           handler: (data) => {
-          this.insertDatos(data.RolPersona, data.Estado);
-          console.log(data);
+            this.insertDatos(data.RolPersona, data.Estado);
+            console.log(data);
           },
         }
       ]
@@ -50,77 +51,73 @@ export class RolPage implements OnInit {
     await alert.present();
   }
 
-
-  ngOnInit() {
-    this.ionViewDidEnter();
-  }
-
-  //consultar Datos
-  ionViewDidEnter(){
-
+ //consultar Datos
+  async ngOnInit() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando..',
+    });
+    await loading.present();
     this.Service.Obtenerdatos()
-    .subscribe(
-     (data) => { this.usuarios = data; 
-       console.log(data);
-       console.log('funciona');
-     },
-     (error) =>{
-       console.log(error);
-     }
-   ) 
-
+      .subscribe( async (data) => {
+          this.usuarios = data;
+          console.log(data);
+          console.log('funciona');
+          await loading.dismiss();
+        }
+      );
+    //this.ionViewDidEnter();
   }
 
   //insertar Datos
-  insertDatos(RolPersona: string, Estado: number){
-   const Rol1 = {
-     //variables iguales a la interface
-    RolPersona,
-    Estado,
-   };
-   this.Service.insertarRol(Rol1).subscribe((newRol) =>{
-    this.usuarios.push(newRol);
-    this.presentToast('Rol Creado');
-   });
+  insertDatos(RolPersona: string, Estado: number) {
+    const Rol1 = {
+      //variables iguales a la interface
+      RolPersona,
+      Estado,
+    };
+    this.Service.insertarRol(Rol1).subscribe((newRol) => {
+      this.usuarios.push(newRol);
+      this.presentToast('Rol Creado');
+    });
   }
 
-  actualizarDatos(idRolPersona:number, RolPersona: string, Estado: number){
+  actualizarDatos(idRolPersona: number, RolPersona: string, Estado: number) {
     const Rol1 = {
-     idRolPersona,
-     RolPersona,
-     Estado,
+      idRolPersona,
+      RolPersona,
+      Estado,
     };
-    this.Service.Actualizar(Rol1).subscribe((newRol) =>{
-     this.usuarios.push(newRol);
+    this.Service.Actualizar(Rol1).subscribe((newRol) => {
+      this.usuarios.push(newRol);
     });
-   }
+  }
 
-   async openAlert1 () {
+  async openAlert1() {
     const alert = await this.alertCtrl.create({
       header: 'Actualizar un rol',
-      inputs:[
+      inputs: [
         {
           name: 'RolPersona',
           type: 'text',
-          placeholder:'Tipo de Rol',
+          placeholder: 'Tipo de Rol',
         },
         {
           name: 'Estado',
           type: 'number',
-          placeholder:`Estado 1 Activo, 0 Inactivo`,
+          placeholder: `Estado 1 Activo, 0 Inactivo`,
         },
       ],
       buttons: [
         {
           text: 'Cancelar',
-          role:'cancel',
+          role: 'cancel',
           cssClass: 'secondary',
         },
         {
           text: 'Actualizar',
           handler: (data) => {
-          this.actualizarDatos(data.idRolPersona, data.RolPersona, data.Estado);
-          console.log(data);
+            this.actualizarDatos(data.idRolPersona, data.RolPersona, data.Estado);
+            console.log(data);
           },
         }
       ]
@@ -128,13 +125,22 @@ export class RolPage implements OnInit {
     await alert.present();
   }
 
-//mensajes
-async presentToast(message: string) {
-  const toast = await this.toastCtrl.create({
-    message,
-    duration: 3000
-  });
-  await toast.present();
-}
+  //mensajes
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 3000
+    });
+    await toast.present();
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando..',
+      duration: 2000
+    });
+    await loading.present();
+    return loading;
+  }
 
 }
--- src/app/servicio.service.ts
+++ src/app/servicio.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { HttpClientModule } from "@angular/common/http";
-import {Rol} from './interfaces/rol';
+import { Rol } from './interfaces/rol';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioService {
-  api ='http://localhost:3000';
+  api = 'http://localhost:3000';
 
   constructor(public http: HttpClient) {
     console.log("hello service");
-   }
-   Obtenerdatos(){
-     const path = `${this.api}/rol`; 
+  }
+  Obtenerdatos() {
+    const path = `${this.api}/rol`;
     return this.http.get<Rol[]>(path);
-   }
+  }
 
-   insertarRol(rol: Rol){
-    const path = `${this.api}/RolInsert`; 
-     return this.http.post<Rol>(path, rol);
-   }
+  insertarRol(rol: Rol) {
+    const path = `${this.api}/RolInsert`;
+    return this.http.post<Rol>(path, rol);
+  }
 
-  Actualizar(rol: Rol){
+  Actualizar(rol: Rol) {
     const path = `${this.api}//RolUpdate/${rol.idRolPersona}`;
     return this.http.put<Rol>(path, rol);
   }
